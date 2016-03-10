4752701ba6b9e2f1bb2f59abdcf49ca7b9d48ab8
Thomas Boerger <tboerger@suse.de>
2016-03-10 10:37:09 +0100

    Added missing comments

--- solder/types.go
+++ solder/types.go
@@ -1,31 +1,41 @@
 package solder
 
+// Build represents a build API response.
 type Build struct {
 }
 
+// Client represents a client API response.
 type Client struct {
 }
 
+// Forge represents a forge API response.
 type Forge struct {
 }
 
+// Key represents a key API response.
 type Key struct {
 }
 
+// Minecraft represents a minecraft API response.
 type Minecraft struct {
 }
 
+// Mod represents a mod API response.
 type Mod struct {
 }
 
+// Pack represents a pack API response.
 type Pack struct {
 }
 
+// User represents a user API response.
 type User struct {
 }
 
+// Version represents a version API response.
 type Version struct {
 }
 
+// Profile represents a profile API response.
 type Profile struct {
 }
