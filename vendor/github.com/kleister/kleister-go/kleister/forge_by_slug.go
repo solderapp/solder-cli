d5c47dd60180b0d5712127a351c2dc3395834d2e
Thomas Boerger <tboerger@suse.de>
2016-09-21 17:46:34 +0200

    Added more required vendored libs

---
+++ vendor/github.com/kleister/kleister-go/kleister/forge_by_slug.go
@@ -0,0 +1,30 @@
+package kleister
+
+import (
+	"github.com/hashicorp/go-version"
+)
+
+// ForgeBySlug sorts a list of forge by slug field.
+type ForgeBySlug []*Forge
+
+// Len is part of the forge sorting algorithm.
+func (u ForgeBySlug) Len() int {
+	return len(u)
+}
+
+// Swap is part of the forge sorting algorithm.
+func (u ForgeBySlug) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+// Less is part of the forge sorting algorithm.
+func (u ForgeBySlug) Less(i, j int) bool {
+	v1, err1 := version.NewVersion(u[i].Slug)
+	v2, err2 := version.NewVersion(u[j].Slug)
+
+	if err1 != nil || err2 != nil {
+		return u[i].Slug < u[j].Slug
+	}
+
+	return v1.LessThan(v2)
+}
